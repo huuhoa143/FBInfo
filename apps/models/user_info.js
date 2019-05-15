edc64464fafa0d161c5ba05194107b89890577af
Nguyễn Hợp Quang <nguyenhopquang13031999@gmail.com>
2019-05-15 12:55:00 +0700

    ok

--- apps/models/user_info.js
+++ apps/models/user_info.js
@@ -1,81 +1,66 @@
 var fs = require('fs'),
-    es = require('event-stream')
+    es = require('event-stream');
+    path = require('path');
+
 const {promisify}= require("util");
 const readFile = promisify(fs.readFile);
 const { getInfoListUid, getInfoUid } = require("../services/get-uids");
-const UIDInfo = require('../models/UIDInfo.models');
+const BASE_PATH = '/home/nguyenhopquang/nodejs/FBInfo/apps/models/uidphone';
+const RESULT_FILE = "result.json";
+const ERROR_FILE = "error.txt";
+const MAX_UID_LENGTH = 200;
 
-const { fail, success } = require('../../utils/response-utils');
-const folder = '/home/huuhoa/Documents/vnphones_output';
-
-
-function CheckDir() {
-    fs.readdir(folder, (err, files) => {
-        files.forEach(file => {
-            console.log(file);
+function walk(currentDirPath, callback) {
+    fs.readdir(currentDirPath, function (err, files) {
+        if (err) {
+            throw new Error(err);
+        }
+        files.forEach(function (name) {
+            var filePath = path.join(currentDirPath, name);
+            var stat = fs.statSync(filePath);
+            if (stat.isFile()) {
+                callback(filePath, stat);
+            } else if (stat.isDirectory()) {
+                walk(filePath, callback);
+            }
         });
     });
 }
 
-async function ReadFile() {
-    let infoUID = "";
+walk(BASE_PATH, async function(filePath) {
+    console.log("Running...");
+    await ReadFile(filePath);
+});
+async function ReadFile(file) {
     var totalLines = 0;
     let listUID = [];
-    let arr = [];
-    let count = 0;
-    var s = fs
-        .createReadStream('/home/huuhoa/WebstormProjects/BookStore/apps/models/test.txt')
-        .pipe(es.split())
-        .pipe(es.mapSync(function (line) {
+    let lineInFile = [];
+    var s = fs.createReadStream(file).pipe(es.split()).pipe(es.mapSync(function (line) {
                     s.pause();
+                    if (line) {
+                        totalLines++;
 
-                    totalLines++;
-
-                    var data = line.split("\n");
-                    Promise.all(data.map(async tmp => {
-                        tmp = tmp.split("\t");
-                        listUID.push(tmp[1]);
-                        // if (count <= 700) {
-                        //     count ++;
-                        //     listUID.push(tmp[1]);
-                        // } else {
-                        //     let info = await getInfoListUid(listUID, "EAAAAUaZA8jlABADPGSFW5tXEkuZB6ZBbl94qEVCjtH1xV4CJEliYnES6ZBRpyTFCAZA42XQNPsuaQA1xm1dCseaKzn8TxO5UXGiKFZAD54PtIhgo6ke6JTDjQEqNF53UuaDx1Vs9mHvwoSK2DsnNIqVytVanEEKYlH75IGgEscUgZDZD");
-                        //     listUID = [];
-                        //     listUID.push(tmp[1]);
-                        //     count = 0;
-                        //     console.log(info)
-                        // }
-                        // console.log("UID: ", tmp[1]);
-                        // let info = await getInfoUid(tmp[1], "EAAAAUaZA8jlABADPGSFW5tXEkuZB6ZBbl94qEVCjtH1xV4CJEliYnES6ZBRpyTFCAZA42XQNPsuaQA1xm1dCseaKzn8TxO5UXGiKFZAD54PtIhgo6ke6JTDjQEqNF53UuaDx1Vs9mHvwoSK2DsnNIqVytVanEEKYlH75IGgEscUgZDZD");
-                        // if (info.hasOwnProperty("gender")) {
-                        //
-                        //     let uidInfo = new UIDInfo({
-                        //         data: info
-                        //     });
-                        //     uidInfo.save()
-                        //         .then(res => {
-                        //             count++;
-                        //             console.log("Update UID: " + tmp[1] + " Line: " + count);
-                        //         })
-                        //         .catch(err => {
-                        //             console.log("Import Err");
-                        //         })
-                        // }
-
-                    }));
-
+                        var data = line.split("\n");
+                        Promise.all(data.map(async tmp => {
+                            tmp = tmp.split("\t");
+                            lineInFile.push({
+                                phone: tmp[0],
+                                uid: tmp[1]
+                            });
+                            listUID.push(tmp[1]);
+                        }));
+                    }
                     s.resume();
                 })
                 .on('error', function (err) {
                     console.log('Error while reading file. ', err);
                 })
                 .on('end',async function () {
-                    console.log('Read entire file');
                     let flag = 0;
                     let split700UID = [];
                     for (let i = 0; i < listUID.length; i++) {
                         let arr = [];
-                        if (i % 200 === 0) {
+                        if (i % MAX_UID_LENGTH === 0) {
                             arr = listUID.slice(flag, i + 1);
                             flag = i + 1;
                             split700UID.push(arr);
@@ -84,63 +69,36 @@ async function ReadFile() {
                     if (listUID.length + 1 !== flag) {
                         split700UID.push(listUID.slice(flag, listUID.length));
                     }
-
-                    let arrInfo = [];
                     for (let i = 0; i < split700UID.length; i++) {
-                        console.log(i, split700UID[i].length);
+                        await fs.writeFile(ERROR_FILE, JSON.stringify(file), err => {
+                            if (err) throw err;
+                        });
                         let info = await getInfoListUid(split700UID[i], "EAAAAUaZA8jlABADPGSFW5tXEkuZB6ZBbl94qEVCjtH1xV4CJEliYnES6ZBRpyTFCAZA42XQNPsuaQA1xm1dCseaKzn8TxO5UXGiKFZAD54PtIhgo6ke6JTDjQEqNF53UuaDx1Vs9mHvwoSK2DsnNIqVytVanEEKYlH75IGgEscUgZDZD");
                         if (info.error) {
-                            console.log("Error: ", i);
-                            Promise.all(split700UID[i].map(async uid => {
-                                let a = await getInfoUid(uid, "EAAAAUaZA8jlABADPGSFW5tXEkuZB6ZBbl94qEVCjtH1xV4CJEliYnES6ZBRpyTFCAZA42XQNPsuaQA1xm1dCseaKzn8TxO5UXGiKFZAD54PtIhgo6ke6JTDjQEqNF53UuaDx1Vs9mHvwoSK2DsnNIqVytVanEEKYlH75IGgEscUgZDZD");
-                                console.log("UID: ", uid);
-                                let infoJSON = JSON.stringify(a) + "\n";
-                                arrInfo.push(infoJSON);
-                            }))
+                            if (!info.error && !info.error.message) {
+                                Promise.all(split700UID[i].map(async uid => {
+                                    let a = await getInfoUid(uid, "EAAAAUaZA8jlABADPGSFW5tXEkuZB6ZBbl94qEVCjtH1xV4CJEliYnES6ZBRpyTFCAZA42XQNPsuaQA1xm1dCseaKzn8TxO5UXGiKFZAD54PtIhgo6ke6JTDjQEqNF53UuaDx1Vs9mHvwoSK2DsnNIqVytVanEEKYlH75IGgEscUgZDZD");
+                                    if (!a.error) {
+                                        let infoJSON = JSON.stringify(a) + "\n";
+                                        // File muốn write vào
+                                        await fs.appendFile(RESULT_FILE, infoJSON, err => {
+                                            if (err) throw err;
+                                        });
+                                    }
+                                }))
+                            }
                         }
-                        // info = JSON.stringify(info);
-                        // let arrInfo = [];
-                        // // Convert kết quả trả về từ object sang array
-                        // Object.keys(info).map( (key, index) => {
-                        //     let _info = {...info[key], id: key, fb_id: key};
-                        //     arrInfo = arrInfo.concat([_info]);
-                        // });
                         for (var property in info) {
                             if (info.hasOwnProperty(property)) {
                                 let abc = info[property];
                                 let abcd = JSON.stringify(abc) + "\n";
-                                arrInfo.push(abcd);
-
-                                // console.log(property, info[property]);
-                                // // Do things here
-                                // await fs.writeFileSync("abc.json", abcd, err => {
-                                //     if (err) throw err;
-                                //     console.log("Write file success");
-                                // });
+                                // File muốn write vào
+                                await fs.appendFile(RESULT_FILE, abcd, err => {
+                                    if (err) throw err;
+                                });
                             }
                         }
-                        // arrInfo.map( infoUId => {
-                        //     let abc = JSON.stringify(infoUId);
-                        //     fs.appendFile("abc.json", abc, err => {
-                        //         if (err) throw err;
-                        //         console.log("Write file success");
-                        //     });
-                        // });
-
-                        // arrInfo.push(info);
                     }
-                    // console.log(arrInfo);
-                    Promise.all(arrInfo.map(async info => {
-                        // File muốn write vào
-                        await fs.appendFile("abc.json", info, err => {
-                            if (err) throw err;
-                            console.log("Write file success");
-                        });
-                    }));
-
                 })
         )
-
 }
-
-ReadFile();
